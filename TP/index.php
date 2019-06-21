36cf02685165487184c5c97056c9fb7c67f334b9
syn <vicdemilly@gmail.com>
2019-06-21 13:39:26 +0200

    Calendrier fini

--- TP/index.php
+++ TP/index.php
@@ -10,7 +10,7 @@
   </head>
   <body>
       <!-- container Bootstrap -->
-      <div class="container-fluid">
+      <div class="container-fluid text-center">
           <!-- header -->
           <header>
             <div class="jumbotron jumbotron-fluid">
@@ -23,112 +23,112 @@
           </header>
           <!-- main -->
           <main>
-              <form>
-                  <select>
-                      <option>Janvier</option>
-                      <option>Février</option>
-                      <option>Mars</option>
-                      <option>Avril</option>
-                      <option>Mai</option>
-                      <option>Juin</option>
-                      <option>Juillet</option>
-                      <option>Août</option>
-                      <option>Septembre</option>
-                      <option>Octobre</option>
-                      <option>Novembre</option>
-                      <option>Décembre</option>
-                  </select>
-                  <?php
-                    // Variable qui ajoutera l'attribut selected de la liste déroulante
-                    $selected = '';
-
-                    // Parcours du tableau
-                    echo '<select name="annees">',"\n";
-                    for($i=1900; $i<=2019; $i++)
-                    {
-                      // L'année est-elle l'année courante ?
-                      if($i == date('Y'))
-                      {
-                        $selected = ' selected="selected"';
-                      }
-                      // Affichage de la ligne
-                      echo "\t",'<option value="', $i ,'"', $selected ,'>', $i ,'</option>',"\n";
-                      // Remise à zéro de $selected
-                      $selected='';
+              <div class="justify-content-center">
+                <?php
+                    // Tableaux représentant les valeurs des mois et des jours de la semaine
+                    $months = array('','Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre');
+                    $days = array('Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche');
+                    // $dayOfWeek vaudra entre 1 (Lundi) et 7 (Dimanche), on l'utilisera comme index pour $days
+                    $dayOfWeek = date("N", mktime(0, 0, 0, $_POST['month'], 1, $_POST['years'])); //(heure, minute, seconde, mois, jour, année)
+                    // Mois et année choisi dans le formulaire
+                    $monthChosen = $_POST['month'];
+                    $yearChosen = $_POST['years'];
+                    $day = 0;
+                    // Nombre de jour pour le mois et l'année choisi (dans un if afin d'éviter une erreur)
+                    if(isset($monthChosen) && isset($yearChosen)) {
+                        $numberOfDay = cal_days_in_month(CAL_GREGORIAN, $monthChosen, $yearChosen);
+                    }else{
+                        $numberOfDay = cal_days_in_month(CAL_GREGORIAN, date('m',time()), date('Y',time()));
                     }
-                    echo '</select>',"\n";
-                  ?>
-              </form>
-              <table class="table text-center">
-                <caption>Septembre 2019</caption>
-                <thead class="thead-dark">
-                  <tr>
-                    <th>Lundi</th>
-                    <th>Mardi</th>
-                    <th>Mercredi</th>
-                    <th>Jeudi</th>
-                    <th>Vendredi</th>
-                    <th>Samedi</th>
-                    <th>Dimanche</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>j1</td>
-                    <td>j2</td>
-                    <td>j3</td>
-                    <td>j4</td>
-                    <td>j5</td>
-                    <td>j6</td>
-                    <td>j7</td>
-                  </tr>
-                  <tr>
-                    <td>j8</td>
-                    <td>j9</td>
-                    <td>j10</td>
-                    <td>j11</td>
-                    <td>j12</td>
-                    <td>j13</td>
-                    <td>j14</td>
-                  </tr>
-                  <tr>
-                    <td>j15</td>
-                    <td>j16</td>
-                    <td>j17</td>
-                    <td>j18</td>
-                    <td>j19</td>
-                    <td>j20</td>
-                    <td>j21</td>
-                  </tr>
-                  <tr>
-                    <td>j22</td>
-                    <td>j23</td>
-                    <td>j24</td>
-                    <td>j25</td>
-                    <td>j26</td>
-                    <td>j27</td>
-                    <td>j28</td>
-                  </tr>
-                  <tr>
-                    <td>j29</td>
-                    <td>j30</td>
-                    <td>j31</td>
-                    <td>j32</td>
-                    <td>j33</td>
-                    <td>j34</td>
-                    <td>j35</td>
-                  </tr>
-                  <tr>
-                    <td>j36</td>
-                    <td>j37</td>
-                    <td>j38</td>
-                    <td>j39</td>
-                    <td>j40</td>
-                    <td>j41</td>
-                    <td>j42</td>
-                  </tr>
-                </tbody>
-              </table>
+                ?>
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST" class="mb-3">
+                    <!-- Label pour les différents mois -->
+                    <label for="month"></label>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-sm-5 col-md-4 col-lg-3">
+                            <select name="month" id="month" class="custom-select">
+                                <?php
+                                // On parcourt le tableau contenant les mois afin de remplir les options
+                                foreach($months as $key => $value){ ?>
+                                    <option value="<?= $key ?>"><?= $value ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <!-- Label pour les années -->
+                        <label for="years"></label>
+                        <div class="col-sm-5 col-md-4 col-lg-3">
+                            <select name="years" id="years" class="custom-select">
+                                <?php
+                                // On boucle pour créer les <option> avec les différentes années
+                                for($year = 1900; $year <= 2019; $year++){
+                                ?>
+                                    <option value="<?= $year ?>"
+                                        <?php if ($year == date('Y')) { ?>
+                                                selected
+                                        <?php } ?>
+                                    >
+                                        <?= $year ?>
+                                    </option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </div>
+                    <p><button type="submit" class="btn btn-success mt-4">Valider</button></p>
+                </form>
+                <p>
+                    <?php if(isset($monthChosen) && isset($yearChosen)){
+                        echo 'Calendrier du mois de ' . $months[$monthChosen+0] . ' ' . $yearChosen;
+                        }else{
+                            echo 'Calendrier du mois de ' . $months[date('m') - 0] . ' ' . date('Y');
+                        }
+                    ?>
+                </p>
+                <table class="table table-bordered">
+                    <thead class="thead bg-info">
+                        <tr>
+                            <?php
+                            // Boucle pour afficher tous les jours au haut du tableau
+                            foreach($days as $value){ ?>
+                            <td class="text-white font-weight-bold"><?= $value ?></td>
+                            <?php } ?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // $test sert à savoir si on a rencontré le jour de la semaine correspondant au jour 1
+                        $test = false;
+                        // Boucle pour 6 ligne de calendrier
+                        for($j = 1; $j <= 6; $j++){
+                            // Si le nombre de jour que l'on a renseigné est inférieur au nombre de jour du mois
+                            if($day < $numberOfDay){
+                            ?>
+                                <tr>
+                                <?php
+                                // Boucle pour les 7 colonnes du calendrier (de lundi à dimanche)
+                                for ($i = 1; $i <= 7; $i++) {
+                                    /* Si nous sommes dans la colonne correspondant au premier jour du mois
+                                      ou que le test est vrai (que le premier jour a été rencontré)
+                                      et que nous n'avons pas dépassé le nombre de jour du mois */
+                                    if(($i == $dayOfWeek || $test == true) && $day < $numberOfDay){
+                                        // On passe le test à true : on a rencontré le premier jour
+                                        $test = true;
+                                        // On incrémente $day pour afficher (1, 2, 3, 4, 5, ...)
+                                        $day++;
+                                        ?>
+                                        <!-- Puis on l'affiche -->
+                                        <td><?= $day ?></td>
+                                <?php
+                                    // Sinon nous sommes soit avant le premier jour, soit après le dernier jour du mois
+                                    } else {
+                                        // Et on affiche une case grisée
+                                        ?>
+                                        <td class="bg-secondary"></td>
+                                        <?php }} ?>
+                                </tr>
+                        <?php }} ?>
+                    </tbody>
+                </table>
+              </div>
           </main>
           <!-- footer -->
           <footer>
